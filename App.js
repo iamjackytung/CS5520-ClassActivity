761e788b50e96f2c85d2143c940def42e3e902be
Jacky Tung <jackyyet@usc.edu>
2023-02-18 16:32:51 -0800

    components in App.js added

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import React from "react";
+import Home from "./Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import GoalDetails from "./components/GoalDetails";
+import Help from "./components/Help";
+import { FontAwesome } from "@expo/vector-icons";
+import PressableButton from "./components/PressableButton";
 
+const Stack = createNativeStackNavigator();
+// console.log(Stack);
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "purple",
+          },
+          headerTintColor: "#eee",
+          headerTitleStyle: {
+            fontSize: 20,
+          },
+        }}
+      >
+        <Stack.Screen
+          options={{
+            title: "All My Goals",
+          }}
+          name="Home"
+          component={Home}
+        />
+        <Stack.Screen
+          // the function in options will recieve route and navigation automatically
+          // options={({ route }) => {
+          //   // console.log(data);
+          //   return {
+          //     // make the title dynamically set using route.params
+
+          //     title: route.params.goalItem.text,
+          //     headerRight: () => {
+          //       // return <Text>testing </Text>;
+          //       // return a pressable button with an icon
+          //       return (
+          //         // <PressableButton
+          //         //   style={{ backgroundColor: "purple" }}
+          //         //   pressHandler={iconPressed}
+          //         // >
+          //         <FontAwesome
+          //           name="warning"
+          //           size={24}
+          //           color="#eee"
+          //           onPress={iconPressed}
+          //         />
+          //         // </PressableButton>
+          //       );
+          //     },
+          //   };
+          // }}
+          name="GoalDetails"
+          component={GoalDetails}
+        />
+        <Stack.Screen name="Help" component={Help} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
